2936098ba6e0216ca7af2927edf8e1c37e245d80
Taehyun Kim <thkim0022@icloud.com>
2022-12-14 18:43:16 +0900

    Update terraform/modules/lambda/iam_role.tf

--- terraform/modules/lambda/iam_role.tf
+++ terraform/modules/lambda/iam_role.tf
@@ -1,42 +1,3 @@
-resource "aws_iam_policy" "this" {
-  description = "IAM Policy"
-  name = "iam-policy-${var.lambda_function_name}"
-
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-              "Effect": "Allow",
-              "Action": [
-                  "s3:ListAllMyBuckets",
-                  "s3:GetBucketLocation"
-              ],
-              "Resource": "*"
-          },
-          {
-              "Effect": "Allow",
-              "Action": "s3:*",
-              "Resource": [
-                  "arn:aws:s3:::${var.s3_bucket_name}",
-                  "arn:aws:s3:::${var.s3_bucket_name}/*"
-              ]
-          },
-          {
-            "Action": [
-              "autoscaling:Describe*",
-              "cloudwatch:*",
-              "logs:*",
-              "sns:*"
-            ],
-            "Effect": "Allow",
-            "Resource": "*"
-          }
-    ]
-  }
-  EOF
-}
-
 resource "aws_iam_role" "this" {
   name = "iam-role-${var.lambda_function_name}"
 
@@ -57,12 +18,42 @@ resource "aws_iam_role" "this" {
   EOF
 }
 
+resource "aws_iam_role_policy" "this" {
+  name = "iam-role-policy-${var.lambda_function_name}"
+  role = aws_iam_role.this.id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "kms:Decrypt",
+          "kms:GenerateDataKey",
+          "s3:ListBucket",
+          "s3:HeadObject",
+          "s3:GetObject",
+          "s3:GetObjectVersion"
+        ],
+        "Resource": [
+          "*",
+        ]
+      }
+    ]
+  }
+  EOF
+
+  depends_on = [
+    aws_iam_role.this
+  ]
+}
+
 resource "aws_iam_role_policy_attachment" "this" {
   role       = aws_iam_role.this.name
   policy_arn = aws_iam_policy.this.arn
 
   depends_on = [
-    aws_iam_policy.this,
-    aws_iam_role.this
+    aws_iam_role_policy.this
   ]
 }
