13e3a4b6389dbe8ddc092e635cae9d4a1fa3dece
Taehyun Kim <thkim0022@icloud.com>
2022-12-15 13:50:36 +0900

    Update terraform/s3/main.tf

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -1,12 +1,12 @@
 module "kms" {
-  source = "./_module/kms"
+  source = "../_module/kms"
   kms_alias_name = var.s3_bucket_name
 
   tags = var.tags
 }
 
 module "s3" {
-  source = "./_module/s3"
+  source = "../_module/s3"
   s3_bucket_name = var.s3_bucket_name
   kms_alias_id = module.kms.kms_alias_id
 
