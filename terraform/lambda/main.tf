abb4de962de858d445d6135ddd6345f290666549
Taehyun Kim <thkim0022@icloud.com>
2022-12-15 15:46:31 +0900

    Update the Terraform Code for Lambda

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -2,11 +2,7 @@ module "lambda" {
   source = "../_module/lambda"
 
   lambda_function_name = var.lambda_function_name
-  s3_bucket_name = module.s3.s3_bucket_name
-
-  depends_on = [
-    module.s3
-  ]
+  s3_bucket_name       = var.s3_bucket_name
 
   tags = var.tags
 }
