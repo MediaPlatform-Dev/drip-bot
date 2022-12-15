133c27bd66f841766df6d5176bd11c0d43ad8e44
Taehyun Kim <thkim0022@icloud.com>
2022-12-15 15:26:04 +0900

    Update terraform/s3

--- terraform/s3/terraform.auto.tfvars
+++ terraform/s3/terraform.auto.tfvars
@@ -1,6 +1,9 @@
 # General
 region      = "ap-northeast-2"
 
+# kms
+kms_deletion_window_in_days = 7
+
 # s3
 s3_bucket_name = "reaction-bot"
 
--- terraform/s3/variables.tf
+++ terraform/s3/variables.tf
@@ -3,6 +3,8 @@ variable "region" {
   type        = string
 }
 
+variable "kms_deletion_window_in_days" {}
+
 variable "s3_bucket_name" {}
 
 variable "tags" {}
